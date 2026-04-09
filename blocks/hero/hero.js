45980c4d82b2e911815c844f5c725dd1f8665f7a
Nivedita Nadkarni <nnadkarni@adobe.com>
2026-04-09 13:18:08 +0530

    fix hero block: remove empty heading wrapper around picture

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,7 +1,7 @@
 export default function decorate(block) {
   const picture = block.querySelector('picture');
   if (picture) {
-    picture.closest('p')?.remove();
+    picture.closest('p, h1, h2, h3')?.remove();
     block.prepend(picture);
   }
 }
